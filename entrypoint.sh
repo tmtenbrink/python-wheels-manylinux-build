756e4732cf923efafeb109a2ec070ee920070dd1
tmtenbrink <64082573+tmtenbrink@users.noreply.github.com>
2021-09-21 12:56:10 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,11 @@
 set -e -x
 
 # CLI arguments
-PY_VERSIONS=$1
-BUILD_REQUIREMENTS=$2
+PY_INSTALL=$1
+PY_VERSIONS=$2
 SYSTEM_PACKAGES=$3
 PRE_BUILD_COMMAND=$4
 PACKAGE_PATH=$5
-PIP_WHEEL_ARGS=$6
 
 # Temporary workaround for LD_LIBRARY_PATH issue. See
 # https://github.com/RalfG/python-wheels-manylinux-build/issues/26
@@ -23,20 +22,19 @@ if [ ! -z "$PRE_BUILD_COMMAND" ]; then
     $PRE_BUILD_COMMAND || { echo "Pre-build command failed."; exit 1; }
 fi
 
+# Install Rust
+curl https://sh.rustup.rs -sSf | sh -s -- -y || { echo "Install Rust failed."; exit 1; }
+
+/opt/python/"${PY_INSTALL}"/bin/pip install --no-cache-dir virtualenv || { echo "Installing virtualenv failed."; exit 1; }
+
+# Install poetry
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py | "/opt/python/${PY_INSTALL}/bin/python" - || { echo "Install poetry failed."; exit 1; }
+
+# Reload path
+. $HOME/.bashrc || { echo "Reload path failed."; exit 1; }
+
 # Compile wheels
-arrPY_VERSIONS=(${PY_VERSIONS// / })
-for PY_VER in "${arrPY_VERSIONS[@]}"; do
-    # Update pip
-    /opt/python/"${PY_VER}"/bin/pip install --upgrade --no-cache-dir pip
-
-    # Check if requirements were passed
-    if [ ! -z "$BUILD_REQUIREMENTS" ]; then
-        /opt/python/"${PY_VER}"/bin/pip install --no-cache-dir ${BUILD_REQUIREMENTS} || { echo "Installing requirements failed."; exit 1; }
-    fi
-
-    # Build wheels
-    /opt/python/"${PY_VER}"/bin/pip wheel . ${PIP_WHEEL_ARGS} || { echo "Building wheels failed."; exit 1; }
-done
+poetry run maturin develop --release -i ${PY_VERSIONS} || { echo "Building wheels failed."; exit 1; }
 
 # Bundle external shared libraries into the wheels
 # find -exec does not preserve failed exit codes, so use an output file for failures
@@ -50,5 +48,5 @@ if [[ -f "$failed_wheels" ]]; then
     exit 1
 fi
 
-echo "Succesfully build wheels:"
+echo "Succesfully built wheels:"
 find . -type f -iname "*-manylinux*.whl"
